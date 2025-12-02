1c3e42c6205281eee460e886c576cc008db8af23
Sameerwevdeveloper786git config --global user.email sameerwebdeveloper41@gmail.com <sameerwebdeveloper41@gmail.com>
2025-12-02 22:55:28 +0500

    sab kuch solve ho gaya ha ab

---
+++ types/react-native/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native' {
+  const RN: any;
+  export = RN;
+}
---
+++ types/react/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'react' {
+  // minimal declarations to satisfy TypeScript until proper @types are installed
+  const react: any;
+  export = react;
+}
